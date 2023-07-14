2ba8e250ff1043bd2994b03ac5856cbd9ea0e6d4
Eunbeen Oh <svbean77@naver.com>
2023-07-14 16:38:16 +0900

    update version to 2.0

--- hello_world_server.py
+++ hello_world_server.py
@@ -7,6 +7,6 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world\n')
+                self.wfile.write(b'Hello world - Version 2.0\n\n')
 httpd = socketserver.TCPServer(('', 8002), Handler)
 httpd.serve_forever()
